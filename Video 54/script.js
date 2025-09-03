6f0df076a265b2f0505c03eb786f6dc6e025b9a3
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-09-03 11:33:35 +0530

    done

---
+++ Video 54/script.js
@@ -0,0 +1,22 @@
+alert("Hello World") ;
+
+console.log("Code is running...") ;
+console.log("Code is also running...") ;
+console.log("Code is looking like a wow...") ;
+
+var a = prompt("Enter your number") ;
+
+console.log("Your number is "+a) ;
+
+var isTrue = confirm("Do you really want to leave this page and blast your computer ?") ;
+
+if (isTrue) {
+    console.log("Computer is blasting") ;
+}
+else {
+    console.log("Computer is not blasting") ;
+}
+
+document.title = "Hello there" ;
+
+// document.body.style.backgroundColor = "rgb(0,110,0)" ;
