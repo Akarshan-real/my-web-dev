fef8b3c5787eb8c42ab1e604a5220461109da743
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-09-15 16:18:21 +0530

    done

---
+++ Video 68/script.js
@@ -0,0 +1,23 @@
+console.log("LESH GOOOOOOOOOOOOOOOOOOOOOOOO") ;
+
+let worldChampion = document.getElementsByClassName("box") ;
+console.log(worldChampion) ;
+
+// worldChampion[2].style.fontSize = "larger" ;
+
+// document.getElementById("two.5").style.fontSize = "2.5rem" ;
+
+// document.querySelector(".box").style.backgroundColor = "green" ;
+
+console.log(document.querySelectorAll(".box")) ;
+
+document.querySelectorAll(".box").forEach (e => {
+    e.style.backgroundColor = "green" ;
+})
+
+console.log(document.getElementsByTagName("div")) ;
+
+console.log(worldChampion[2].matches("#two\\.5")) ;
+console.log(worldChampion[2].closest("html")) ;
+
+console.log(document.querySelector(".container").contains(worldChampion[1])) ;
