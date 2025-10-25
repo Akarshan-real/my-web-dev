3f0d4ff09450a7006fe4752de282b176f57307e5
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-10-25 15:34:09 +0530

    pc

---
+++ Video 87/absolutepath.js
@@ -0,0 +1,11 @@
+import path from "path"
+
+
+let myPath = "e:\\Sigma Web Dev\\Video 87\\absolutepath.js";
+
+console.log(path.extname(myPath));
+
+console.log(path.dirname(myPath));
+console.log(path.basename(myPath));
+
+console.log(path.join("c:/", "programs\\harry.txt"));
