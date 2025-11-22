1ce6c1b7e47fe79b0e8dd71bf9b1a6a529a54c1f
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-11-22 17:17:18 +0530

    pc

--- Video 101/js/script.js
+++ Video 101/js/script.js
@@ -6,6 +6,18 @@
     
 })();
 
+(function hoverColorChange() {
+    {
+        document.querySelectorAll(".blueLogoGlow").forEach(e => {
+            e.addEventListener("mouseenter",()=>{
+                e.style.color = "rgb(29,155,240)";
+            });
+            e.addEventListener("mouseleave",()=>{
+                e.style.color = "rgb(113,118,123)";
+            });
+        })
+    }
+})();
 
 // Delay Function
 (async function main() {
