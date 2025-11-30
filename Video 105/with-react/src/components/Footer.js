157d1caa9b6f0c8e1e5001e1acebeee3094f03d7
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-12-01 01:09:18 +0530

    lenovo

---
+++ Video 105/with-react/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        I am a footer leshgo
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
