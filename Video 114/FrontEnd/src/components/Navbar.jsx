76c813989bbabaf9c74abd8580ef4233d20c2c57
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2025-12-13 22:12:44 +0530

    done

--- Video 114/FrontEnd/src/components/Navbar.jsx
+++ Video 114/FrontEnd/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ todos }) => {
           <span className='w-fit h-fit cursor-context-menu font-bold'>iTask</span>
         </div>
         <ul className='flex justify-center items-center gap-10 h-full'>
-          <li className={`lis cursor-pointer relative h-fit ${show ? "active" : "deactive"}`} onClick={() => setShow(false)}>Home</li>
+          <li className={`lis cursor-pointer relative h-fit ${show ? "active scale-[1.15]" : "deactive"}`} onClick={() => setShow(false)}>Home</li>
           <li className={`lis cursor-pointer relative h-fit ${show ? "deactive" : "active"}`} onClick={() => setShow(true)}>Your Tasks</li>
         </ul>
       </div>
